e7193ffdb2ce07c32b9b17c8b4234767ded1e026
b21sebgr <102599253+b21sebgr@users.noreply.github.com>
2022-05-24 13:39:23 +0200

    better code now i have internet in my power

--- app/src/main/java/com/example/slutprojekt/JsonTask.java
+++ app/src/main/java/com/example/slutprojekt/JsonTask.java
@@ -10,7 +10,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-@SuppressWarnings("deprecation")
 public class JsonTask extends AsyncTask<String, String, String> {
 
     public interface JsonTaskListener {
